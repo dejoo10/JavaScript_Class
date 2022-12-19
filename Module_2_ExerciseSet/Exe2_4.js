536d8b564a8301360e5e6b3f085ab60d56b92387
dejoo10 <daygme@gmail.com>
2022-12-19 18:01:29 +0200

    gave error

---
+++ Module_2_ExerciseSet/Exe2_4.js
@@ -0,0 +1,16 @@
+const num = [];
+                  //This will take imput from the user to initiate the loop
+var user = parseInt(prompt("Enter any number: "));
+                  //loop until 0 is pressed
+while (user !==0) {
+  num[i] = parseInt(prompt("Enter any number: "));
+
+  for(let j=0; j>0; j++) {
+    console.log(list);
+  }
+}
+
+
+
+
+
