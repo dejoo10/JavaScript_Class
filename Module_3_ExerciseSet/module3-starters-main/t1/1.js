639d73da96006a45a4e9306c8b4388148fe98bd0
dejoo10 <daygme@gmail.com>
2022-12-20 11:03:16 +0200

    1 done

---
+++ Module_3_ExerciseSet/module3-starters-main/t1/1.js
@@ -0,0 +1,6 @@
+//This code will add the list unoderly to the html code
+const div = document.querySelector("#target");
+const html = "<li>First item</li>" +
+    "<li>Second item</li>" +
+    "<li>Third item</li>";
+div.innerHTML=html;
