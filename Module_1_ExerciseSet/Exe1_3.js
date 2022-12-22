bca96622279efe7fcfaf1255e2d2df2d28bac47a
dejoo10 <daygme@gmail.com>
2022-12-22 19:56:24 +0200

    tried new things

--- Module_1_ExerciseSet/Exe1_3.js
+++ Module_1_ExerciseSet/Exe1_3.js
@@ -1,16 +1,18 @@
 
-
+//This part will promt the user for numbers//
 var num1 = parseInt(prompt("Enter the first number: "));
 var num2 = parseInt(prompt("Enter the second number: "));
 var num3 = parseInt(prompt("Enter the third number: "));
 var div = 3;
 
+//here, the values are stored//
 var num_sum = num1+num2+num3;
 var prod = num1*num2*num3;
 var ave = (num1+num2+num3 )/div;
 
-  document.getElementById("Sum").innerText = "Sum, " + num_sum;
+//this will print the result to the html//
+  document.getElementById("Sum").innerText = "Sum= " + num_sum;
 
-  document.getElementById("Prod").innerText = "Prod, " + prod;
+  document.getElementById("Prod").innerText = "Product= " + prod;
 
-  document.getElementById("Ave").innerText = "Ave, " + ave;
+  document.getElementById("Ave").innerText = "Average= " + ave;
